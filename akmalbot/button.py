2f06f08c839d07c481f1f4ae15dedfe879594bfb
YMirodilbek <101879531+YMirodilbek@users.noreply.github.com>
2025-06-19 09:19:17 +0500

    Update button.py

--- akmalbot/button.py
+++ akmalbot/button.py
@@ -9,10 +9,10 @@ inline_uz_ru = builder.as_markup()
 
 builder_text_uz = InlineKeyboardBuilder()
 builder_text_uz.row(InlineKeyboardButton(text="💊 Dori haqida ma'lumot olish", callback_data="dori_info"))
+builder_text_uz.row(InlineKeyboardButton(text="👩‍💻 Operator bilan Aloqa", callback_data="aloqa"))
 builder_text_uz.row(InlineKeyboardButton(text="👨‍⚕ Shifokor bilan maslahat", callback_data="maslahat"))
 builder_text_uz.row(InlineKeyboardButton(text="🧑‍⚕ Diagnostika", callback_data="diagnostika"))
 builder_text_uz.row(InlineKeyboardButton(text="🚚 Yetkazib berish",url="https://akmalfarm.uz"))
-builder_text_uz.row(InlineKeyboardButton(text="👨‍💼Admin bilan Aloqa", callback_data="aloqa"))
 builder_text_uz.row(InlineKeyboardButton(text="🏥 Bizning dorixonalar", callback_data="dorihona_uz"))
 builder_text_uz.row(InlineKeyboardButton(text="❓ Boshqa savol", callback_data="other"))
 inline_uz_text = builder_text_uz.as_markup()
@@ -20,6 +20,7 @@ inline_uz_text = builder_text_uz.as_markup()
 
 builder_text_ru = InlineKeyboardBuilder()
 builder_text_ru.row(InlineKeyboardButton(text="💊 Получить информацию о лекарствах", callback_data="dd"))
+builder_text_ru.row(InlineKeyboardButton(text="👩‍💻 Чат с оператором", callback_data="aloqa"))
 builder_text_ru.row(InlineKeyboardButton(text="👨‍⚕ Консультация врача", callback_data="maslahat"))
 builder_text_ru.row(InlineKeyboardButton(text="🧑‍⚕ Диагностика", callback_data="diagnostika"))
 builder_text_ru.row(InlineKeyboardButton(text="🚚 Доставка", callback_data="delivery"))
@@ -27,6 +28,10 @@ builder_text_ru.row(InlineKeyboardButton(text="🏥 Наши аптеки", call
 builder_text_ru.row(InlineKeyboardButton(text="❓ Другой вопрос", callback_data="other"))
 
 inline_ru_text = builder_text_ru.as_markup()
+
+
+
+
 
 
 go_back = InlineKeyboardMarkup(inline_keyboard=[
