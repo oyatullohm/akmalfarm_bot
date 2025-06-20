ae80cd81486e704d53bc3d75b2245cbee5ca7abd
YMirodilbek <101879531+YMirodilbek@users.noreply.github.com>
2025-06-20 09:21:24 +0500

    Update button.py

--- akmalbot/button.py
+++ akmalbot/button.py
@@ -8,23 +8,23 @@ builder.row(InlineKeyboardButton(text="🇷🇺  RU", callback_data="lang_ru"))
 inline_uz_ru = builder.as_markup()
 
 builder_text_uz = InlineKeyboardBuilder()
-builder_text_uz.row(InlineKeyboardButton(text="💊 Dori haqida ma'lumot olish", callback_data="dori_info"))#2
-builder_text_uz.row(InlineKeyboardButton(text="👩‍💻 Operator bilan Aloqa", callback_data="aloqa"))#3
-builder_text_uz.row(InlineKeyboardButton(text="👨‍⚕ Shifokor bilan maslahat", callback_data="maslahat"))#5
-builder_text_uz.row(InlineKeyboardButton(text="🧑‍⚕ Diagnostika", callback_data="diagnostika"))#4
-builder_text_uz.row(InlineKeyboardButton(text="🚚 Yetkazib berish",url="https://akmalfarm.uz"))
-builder_text_uz.row(InlineKeyboardButton(text="🏥 Bizning dorixonalar", callback_data="dorihona_uz"))# 1 
+builder_text_uz.row(InlineKeyboardButton(text="📄 Retsept yuklash", callback_data="aloqa"))#1
+builder_text_uz.row(InlineKeyboardButton(text="🚚 Yetkazib berish",url="https://akmalfarm.uz"))#2
+builder_text_uz.row(InlineKeyboardButton(text="👨‍⚕ Shifokor bilan maslahat", callback_data="maslahat"))#3
+builder_text_uz.row(InlineKeyboardButton(text="👩‍⚕️ Ayollar markazi", callback_data="diagnostika"))#4
+builder_text_uz.row(InlineKeyboardButton(text="🏥 Bizning dorixonalar", callback_data="dorihona_uz"))# 5
+builder_text_uz.row(InlineKeyboardButton(text="💊 Katalog", callback_data="dori_info"))#6
 builder_text_uz.row(InlineKeyboardButton(text="❓ Boshqa savol", callback_data="other"))#7
 inline_uz_text = builder_text_uz.as_markup()
 
 
 builder_text_ru = InlineKeyboardBuilder()
-builder_text_ru.row(InlineKeyboardButton(text="💊 Получить информацию о лекарствах", callback_data="dori_info_ru"))#2
-builder_text_ru.row(InlineKeyboardButton(text="👩‍💻 Чат с оператором", callback_data="aloqa_ru"))#3
-builder_text_ru.row(InlineKeyboardButton(text="👨‍⚕ Консультация врача", callback_data="maslahat_ru"))#5
-builder_text_ru.row(InlineKeyboardButton(text="🧑‍⚕ Диагностика", callback_data="diagnostika_ru"))#4
+builder_text_ru.row(InlineKeyboardButton(text="📄  Отправить рецепт", callback_data="aloqa_ru"))#1
 builder_text_ru.row(InlineKeyboardButton(text="🚚 Доставка",url="https://akmalfarm.uz"))#6
+builder_text_ru.row(InlineKeyboardButton(text="👨‍⚕ Консультация врача", callback_data="maslahat_ru"))#5
+builder_text_ru.row(InlineKeyboardButton(text="👩‍⚕️ Женский центр", callback_data="diagnostika_ru"))#4
 builder_text_ru.row(InlineKeyboardButton(text="🏥 Наши аптеки", callback_data="dorihona_ru"))# 1 
+builder_text_ru.row(InlineKeyboardButton(text="💊 Каталог", callback_data="dori_info_ru"))#2
 builder_text_ru.row(InlineKeyboardButton(text="❓ Другой вопрос", callback_data="other_ru"))#7
 
 inline_ru_text = builder_text_ru.as_markup()
