790e13601af71e95e9437f28bb11b6849a2b8d89
oyatullohm <marzoyahoni@gmail.com>
2025-06-21 11:17:20 +0000

    f

--- akmalbot/state.py
+++ akmalbot/state.py
@@ -33,7 +33,7 @@ from main.models import *
 
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
     
-GROUP_ID = -1002524424597 
+# GROUP_ID = -1002524424597 
 GROUP_ID = -4724451433
 
 TOKEN  = env.str('TOKEN')
